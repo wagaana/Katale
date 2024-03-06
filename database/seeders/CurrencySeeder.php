2201b0155acf823a0f28aa3914801dfecb4bce68
wagaana <wagaanaa@gmail.com>
2024-03-07 01:05:54 +0300

    Marketplace init

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -16,10 +16,25 @@ class CurrencySeeder extends Seeder
         Currency::truncate();
 
         $currenciesData = [
-            ["code" => "UGX", "name" => "Uganda Shilling", "entity" => "Uganda", "country_code" => "UG", "minorUnit" => "0", "userId" => "1"],
-            ["code" => "KES", "name" => "Kenyan Shilling", "entity" => "Kenya", "country_code" => "KE", "minorUnit" => "2", "userId" => "1"],
-            ["code" => "TZS", "name" => "Tanzanian Shilling", "entity" => "Tanzania, United Republic of Tanzania", "country_code" => "TZ", "minorUnit" => "2", "userId" => "1"],
-            ["code" => "RWF", "name" => "Rwanda Franc", "entity" => "Rwanda", "country_code" => "RW", "minorUnit" => "0", "userId" => "1"],
+            [
+                "code" => "UGX", "name" => "Uganda Shilling", "entity" => "Uganda", "country_code" => "UG", "minorUnit" => "0", "userId" => "1",
+                "exchange_rate" => 1.000000, "conversionChargePercentage" => 0.00, "bidAskPercentageBuy" => 0.00, "bidAskPercentageSell" => 0.00, "transactionFee" => 0.00, "buy" => 0.98, "sell" => 1.02
+            ],
+
+            [
+                "code" => "KES", "name" => "Kenyan Shilling", "entity" => "Kenya", "country_code" => "KE", "minorUnit" => "2", "userId" => "1",
+                "exchange_rate" => 25.875150, "conversionChargePercentage" => 0.00, "bidAskPercentageBuy" => 2.00, "bidAskPercentageSell" => 3.00, "transactionFee" => 0.00, "buy" => 25.36, "sell" => 26.65
+            ],
+
+            [
+                "code" => "TZS", "name" => "Tanzanian Shilling", "entity" => "Tanzania, United Republic of Tanzania", "country_code" => "TZ", "minorUnit" => "2", "userId" => "1",
+                "exchange_rate" => 1.490021, "conversionChargePercentage" => 0.00, "bidAskPercentageBuy" => 2.00, "bidAskPercentageSell" => 3.00, "transactionFee" => 0.00, "buy" => 1.46, "sell" => 1.53
+            ],
+
+            [
+                "code" => "RWF", "name" => "Rwanda Franc", "entity" => "Rwanda", "country_code" => "RW", "minorUnit" => "0", "userId" => "1",
+                "exchange_rate" => 3.152729, "conversionChargePercentage" => 0.00, "bidAskPercentageBuy" => 2.00, "bidAskPercentageSell" => 3.00, "transactionFee" => 0.00, "buy" => 3.09, "sell" => 3.25
+            ],
         ];
         collect($currenciesData)->each(function ($settingData) {
             Currency::create($settingData);
